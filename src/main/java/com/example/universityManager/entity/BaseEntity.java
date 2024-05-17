c988d83bcb4c63a28eb749f1572c8965db82fc51
meraj <merajmusavi1243m@gmail.com>
2024-05-17 19:48:03 +0330

    Course entity design

---
+++ src/main/java/com/example/universityManager/entity/BaseEntity.java
@@ -0,0 +1,15 @@
+package com.example.universityManager.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
---
+++ src/main/java/com/example/universityManager/entity/Course.java
@@ -0,0 +1,40 @@
+package com.example.universityManager.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+public class Course {
+
+    @Column(unique = true,nullable = false)
+    private int code;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private int units;
+
+    @ManyToOne
+    @JoinColumn(name = "professor_id")
+    private Professor professor;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "course_student",
+            joinColumns = {@JoinColumn(name = "course_id")},
+            inverseJoinColumns = {@JoinColumn(name = "student_id")}
+    )
+    private Set<Student> students  = new HashSet<>();
+
+}
+
